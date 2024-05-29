f4c7ea95408c71e9fa1d08fa8eb22584601ba1a4
Sukanya <karnsukanya@gmail.com>
2024-05-29 21:31:51 +0530

    Winner declaration added

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ var randomNumber2= Math.floor(Math.random()*6+1);
 var randomDiceImage2= "dice"+ randomNumber2+".png";
 var randomImageSrc2= "images/" + randomDiceImage2;
 var image2= document.querySelectorAll("img")[1].setAttribute("src", randomImageSrc2);
+
+if(randomNumber1> randomNumber2){
+    document.querySelector("h1").innerHTML="Player 1 Wins";
+}else{
+        document.querySelector("h1").innerHTML = "Player 2 Wins";
+
+}
